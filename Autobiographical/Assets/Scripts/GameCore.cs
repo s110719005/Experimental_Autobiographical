92f0c2b4fb58750c0a948af080297c42ae5db9aa
amazingunicornn <alicewang6538@gmail.com>
2024-04-07 02:18:02 -0600

    ADD scene change script

---
+++ Autobiographical/Assets/Scripts/GameCore.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameCore : MonoBehaviour
+{
+    public static GameCore Instance;
+    private void Awake() 
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Stage0Scene");
+    }
+
+    public void LeveSelect()
+    {
+        SceneManager.LoadScene("LevelSelectScene");
+    }
+
+    public void ChangeStage(int stage)
+    {
+        string stageName = "Stage" + stage.ToString() + "Scene";
+        SceneManager.LoadScene(stageName);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
